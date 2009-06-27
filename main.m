3b7769145a112c9c63d6dcb5197e910cbe18ae9a
Matt Wright <matt@Caesium.lan>
2009-06-27 23:48:06 +0100

    Initial blank Xcode project.

---
+++ main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  TeamSquawk
+//
+//  Created by Matt Wright on 27/06/2009.
+//  Copyright Matt Wright Consulting 2009. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc,  (const char **) argv);
+}
