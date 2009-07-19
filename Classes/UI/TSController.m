c6b97e65726909fea504745d9c12a693e6d4e407
Matt Wright <matt@Caesium.lan>
2009-07-19 14:55:24 +0100

    Fixed sparkle versioning

--- Classes/UI/TSController.m
+++ Classes/UI/TSController.m
@@ -6,6 +6,9 @@
 //  Copyright 2009 Matt Wright Consulting. All rights reserved.
 //
 
+#import <Sparkle/Sparkle.h>
+#import "TSStandardVersionComparator.h"
+
 #import <MTCoreAudio/MTCoreAudio.h>
 #import <ExceptionHandling/ExceptionHandling.h>
 
@@ -1458,6 +1461,37 @@ void UncaughtExceptionHandler(NSException *exception)
   {
     [teamspeakConnection disconnect];
   }
+}
+
+#pragma mark Sparkle
+
+NSInteger compareVersions(SUAppcastItem *a, SUAppcastItem *b, void* context)
+{
+  return [(TSStandardVersionComparator*)context compareVersion:[b versionString] toVersion:[a versionString]];
+}
+
+- (SUAppcastItem*)bestValidUpdateInAppcast:(SUAppcast *)appcast forUpdater:(SUUpdater *)bundle;
+{
+  TSStandardVersionComparator *comparison = [[TSStandardVersionComparator alloc] init];
+  NSMutableArray *possibles = [[NSMutableArray alloc] init];
+  
+  NSString *realBundleVersion = [[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleShortVersionString"];
+  
+  for (SUAppcastItem *item in [appcast items])
+  {
+    BOOL ignoreNormally = [[[item propertiesDictionary] objectForKey:@"sparkle:ignorenormally"] isEqualTo:@"1"];
+    
+    if (!ignoreNormally && ([comparison compareVersion:realBundleVersion toVersion:[item versionString]] == NSOrderedAscending))
+    {
+      [possibles addObject:item];
+    }
+  }
+
+  if ([possibles count] > 0)
+  {
+    return [possibles objectAtIndex:0];
+  }
+  return nil;
 }
 
 //- (void)audioDecoderThread2
